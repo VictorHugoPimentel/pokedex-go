6fabe96b38deba8f61411a92610fdf8204c80631
victorhugoYUV <victor.pimentel@yuv.com.br>
2026-07-16 17:47:34 -0300

    Creating cache for user and starting cache refresh

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -1,1 +1,25 @@
 package pokecache
+
+import "time"
+
+type Cache struct {
+	cache map[string]CacheEntry
+}
+
+type CacheEntry struct {
+	val []byte
+	createdAt time.Time
+}
+
+func NewCache() Cache {
+	return Cache{
+		cache: make(map[string]CacheEntry),
+	}
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.cache[key] = CacheEntry{
+		val: val,
+		createdAt: time.Now().UTC(),
+	}
+} 
