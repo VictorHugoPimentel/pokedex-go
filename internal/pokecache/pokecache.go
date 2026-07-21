295675bb5aa74088b54508770d7c37afbca633fc
victorhugoYUV <victor.pimentel@yuv.com.br>
2026-07-21 15:34:35 -0300

    Solved cache concurrent issue

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -1,9 +1,13 @@
 package pokecache
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 type Cache struct {
 	cache map[string]CacheEntry
+	mux sync.Mutex
 }
 
 type CacheEntry struct {
@@ -14,12 +18,15 @@ type CacheEntry struct {
 func NewCache(internal time.Duration) Cache {
 	c := Cache{
 		cache: make(map[string]CacheEntry),
+		mux: sync.Mutex{},
 	}
 	go c.ReapLoop(internal)
 	return c
 }
 
 func (c *Cache) Add(key string, val []byte) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
 	c.cache[key] = CacheEntry{
 		val:       val,
 		createdAt: time.Now().UTC(),
@@ -27,6 +34,8 @@ func (c *Cache) Add(key string, val []byte) {
 }
 
 func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
 	cacheE, ok := c.cache[key]
 	return cacheE.val, ok
 }
@@ -39,6 +48,8 @@ func (c *Cache) ReapLoop(internal time.Duration) {
 }
 
 func (c *Cache) reap(internal time.Duration) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
 	timeAgo := time.Now().UTC().Add(-internal)
 	for k, v := range c.cache {
 		if v.createdAt.Before(timeAgo) {
